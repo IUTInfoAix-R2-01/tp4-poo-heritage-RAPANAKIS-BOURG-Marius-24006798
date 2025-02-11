a7be3137d683f1485b5dd6cd1dfe75ebf099beb6
Marius Rapanakis-Bourg <marius.rapanakis-bourg@etu.univ-amu.fr>
2025-02-11 16:47:52 +0100

    feat: add of Circle and Cylinder + test method

--- src/tp3exosYaip5/n2/Circle.java
+++ src/tp3exosYaip5/n2/Circle.java
@@ -3,4 +3,24 @@ package tp3exosYaip5.n2;
 public class Circle {
 	private double radius;
 	private String color;
+	
+	public Circle(double radius, String color) {
+		this.radius = radius;
+		this.color = color;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + ", color=" + color + "]";
+	}
+
+
+
+	public static void main(String[] args) {
+        Circle circle1 = new Circle(1.0,"rouge");
+
+        System.out.println(circle1);
+    }
 }
--- src/tp3exosYaip5/n2/Cylinder.java
+++ src/tp3exosYaip5/n2/Cylinder.java
@@ -4,8 +4,21 @@ public class Cylinder {
 	private Circle base;
 	private double height;
 	
-	public Cylinder() {
-	      base = new Circle();
+	public Cylinder(double radius, String color) {
+	      base = new Circle(radius, color);
 	      height = 1.0; 
 	   }
+
+	@Override
+	public String toString() {
+		return "Cylinder [base=" + base + ", height=" + height + "]";
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		Cylinder cy1 = new Cylinder(15.0,"bleu");
+		
+		System.out.println(cy1);
+	}
 }
