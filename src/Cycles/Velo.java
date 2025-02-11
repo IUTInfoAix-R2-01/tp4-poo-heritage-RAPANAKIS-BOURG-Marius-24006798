1e2630b1825a4900c78cd8257fde1f5f20e30351
Marius Rapanakis-Bourg <marius.rapanakis-bourg@etu.univ-amu.fr>
2025-02-11 14:56:43 +0100

    feat: separation of VeloElectrique and Velo test in each class main

--- src/Cycles/Velo.java
+++ src/Cycles/Velo.java
@@ -63,32 +63,19 @@ public class Velo {
 	public static void main(String[] args) {
 		Velo v1 = new Velo();
 		Velo v2 = new Velo(10.0,5.0);
-		Velo v3 = new Velo(10.0);
-		
-		VeloElectrique ve1 = new VeloElectrique();
-		VeloElectrique ve2 = new VeloElectrique(10.0,5.0);
-		VeloElectrique ve3 = new VeloElectrique(10.0,5.0,30.0);
-				
+		Velo v3 = new Velo(10.0);			
 				
 		System.out.println('\n');
 		System.out.println(v1);
 		System.out.println(v2);
 		System.out.println(v3);
 		
-		System.out.println('\n');
-		System.out.println(ve1);
-		System.out.println(ve2);
-		System.out.println(ve3);
+		System.out.println(v1.getPuissance(15.0));
+		System.out.println(v2.getPuissance(15.0));
+		System.out.println(v2.getPuissance(15.0));
+
 		
-		System.out.println('\n');
-		System.out.println(ve1.getBraquet());
-		System.out.println(ve2.getBraquet());
-		System.out.println(ve3.getBraquet());
 		
-		System.out.println('\n');
-		System.out.println(ve1.getPuissance(15.0));
-		System.out.println(ve2.getPuissance(15.0));
-		System.out.println(ve3.getPuissance(15.0));
 	}
 
 	
--- src/Cycles/VeloElectrique.java
+++ src/Cycles/VeloElectrique.java
@@ -40,6 +40,32 @@ public class VeloElectrique extends Velo {
 		return super.getPuissance(FrequenceCoupsDePedale) * facteurPuissanceMoteur;
 	}
 	
+	public static void main() {
+		
+		VeloElectrique ve1 = new VeloElectrique();
+		VeloElectrique ve2 = new VeloElectrique(10.0,5.0);
+		VeloElectrique ve3 = new VeloElectrique(10.0,5.0,30.0);
+		
+		System.out.println('\n');
+		System.out.println(ve1);
+		System.out.println(ve2);
+		System.out.println(ve3);
+		
+		System.out.println('\n');
+		System.out.println(ve1.getFacteurPuissanceMoteur());
+		System.out.println(ve2.getFacteurPuissanceMoteur());
+		System.out.println(ve3.getFacteurPuissanceMoteur());
+		
+		System.out.println('\n');
+		System.out.println(ve1.getBraquet());
+		System.out.println(ve2.getBraquet());
+		System.out.println(ve3.getBraquet());
+		
+		System.out.println('\n');
+		System.out.println(ve1.getPuissance(15.0));
+		System.out.println(ve2.getPuissance(15.0));
+		System.out.println(ve3.getPuissance(15.0));
+	}
 	
 
 	
