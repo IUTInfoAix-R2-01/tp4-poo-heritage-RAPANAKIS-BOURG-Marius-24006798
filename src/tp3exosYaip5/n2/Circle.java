9ea75634aa7a9c4d6b74a39b89db7ac7bdb51c24
Marius Rapanakis-Bourg <marius.rapanakis-bourg@etu.univ-amu.fr>
2025-02-11 16:37:04 +0100

    feat: add of Circle and Cylinder class working with composition

---
+++ src/tp3exosYaip5/n2/Circle.java
@@ -0,0 +1,6 @@
+package tp3exosYaip5.n2;
+
+public class Circle {
+	private double radius;
+	private String color;
+}
---
+++ src/tp3exosYaip5/n2/Cylinder.java
@@ -0,0 +1,11 @@
+package tp3exosYaip5.n2;
+
+public class Cylinder {
+	private Circle base;
+	private double height;
+	
+	public Cylinder() {
+	      base = new Circle();
+	      height = 1.0; 
+	   }
+}
